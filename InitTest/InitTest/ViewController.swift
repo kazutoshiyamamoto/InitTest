45859ca01057054bce89f0f99133e5d1e982e16d
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-09-25 23:19:58 +0900

    インスタンスプロパティの定義

--- InitTest/InitTest/ViewController.swift
+++ InitTest/InitTest/ViewController.swift
@@ -8,34 +8,25 @@
 
 import UIKit
 
-class Circle {
-    // 半径
-    var radius: Double = 1.0
-    // 面積
-    var area:Double {
-        // 面積を返す
-        return radius * radius * Double.pi
-    }
+class MyClass {
+    // インスタンスプロパティ
+    let msg: String
+    
 }
 
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let myCircle = Circle()
-        myCircle.radius = 10
-        let menseki = myCircle.area
-        print(menseki)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
